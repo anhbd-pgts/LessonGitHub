6001eb13545127618aa1f02a49f4878251f03ca3
anhbd_pgts <anh.bd@pgt-solutions.io>
2024-12-10 17:02:32 +0700

    deleta appBar

--- lib/UI/Profile.dart
+++ lib/UI/Profile.dart
@@ -6,9 +6,6 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
